367b22e7a29074c1e0f37172ff565b369bce048c
jp71 <japeters71@gmail.com>
2020-04-24 14:44:42 +0100

    Add project files.

---
+++ HelloPi/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloPi
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
